b3e5552116038c2fed216ad8e6e55a26259cae94
Jan-Hettich <janhettich@gmail.com>
2014-03-02 19:46:36 -0800

    cleaned up and commented the steps

--- div3.rb
+++ div3.rb
@@ -9,34 +9,21 @@ class DivisibleBy3
   INPUT_ALPHABET = "01"
 
   def accept?(input)
-    puts "checking input:  #{input}"
+#    puts "checking input:  #{input}"
     raise ArgumentError if (input.chars - INPUT_ALPHABET.chars).count > 0 
-    final_state = input.chars.reduce(START){ |q,c| self.next2(q,c) }
-    p ACCEPT.include?(final_state)
+    final_state = input.chars.reduce(START){ |q,c| self.next(q,c) }
     ACCEPT.include? final_state
   end
 
-  def next0(q,c)
-    0
-  end
-
-  def next1(q,c)
-    d = c.to_i
-    case q
-      when 0 then d
-      when 1 then d^1
-    end
-  end
-
-  def next2(q,c)
+  def next(q,c)
     d = c.to_i
     new_state = case q 
       when 0 then d
       when 1 then (d^1)*2
-      when 2 then 2
+      when 2 then d+1
     end
-    puts new_state
-    new_state
+#    puts new_state
+#    new_state
   end
 
 end
--- spec/div3_spec.rb
+++ spec/div3_spec.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env ruby
 require_relative "../div3"
 
 RSpec::Matchers.define :accept do |input|
@@ -7,52 +6,87 @@ RSpec::Matchers.define :accept do |input|
   end
 end
 
+RSpec::Matchers.define :reject do |input|
+  match do |dfa|
+    !dfa.accept?(input)
+  end
+end
+
 describe DivisibleBy3 do
 
   let (:div3) {DivisibleBy3.new}
 
-  it "has an 'accept?' method" do
-    expect(div3).to respond_to :accept?
-  end
+  it {should respond_to(:accept?)}
 
   it "scans strings consisting of 0's and 1's" do
     ["0", "1", "01", "1001001", "00001010"].each { |s| div3.accept?(s) }
-  end
-
-  it "accepts the empty string" do
-    expect(div3). to accept("")
   end
 
   it "raises error if input string contains characters outside the input alphabet" do
     expect {div3.accept?("12345")}.to raise_error ArgumentError
   end
 
-  it "accepts '0'" do
-    expect(div3).to accept("0")
+  it {should accept ""}
+  # next returs 0
+
+  it "ignores leading 0's" do
+    (0..500).each do |n|
+      expect(div3.accept?("%010b" % n)).to eq(div3.accept?("%b" % n))
+    end
   end
 
-  it "rejects '1'" do
-    expect(div3).not_to accept("1")
+  it {should accept "0"}
+
+  it {should reject "1"}
+  # add state 1, next returns d
+
+  it {should accept "00"}
+
+  it {should reject "01"}
+
+  it {should reject "10"}
+  # next returns d or d^1
+
+  it {should accept "11"}
+
+  it {should reject "100"}
+  # still works
+
+  it {should reject "101"}
+  # add state 2, and when 1 then (d^1)*2
+  # 2 => 2 for now
+
+  it {should accept "110"}
+
+  it {should reject "111"}
+
+  it {should reject "1000"}
+
+  it {should accept "1001"}
+  # 2 => d + 1
+  
+  it "correctly interprets numbers up to (decimal) 500" do
+    (0..500).each do |n|
+      if (n % 3 == 0) 
+        expect(div3).to accept("%b" % n)
+      else 
+        expect(div3).to reject("%b" % n)
+      end 
+    end
   end
 
-  it "accepts '00'" do
-    expect(div3).to accept("00")
-  end
-
-  it "rejects '01'" do
-    expect(div3).not_to accept("01")
-  end
-
-  it "rejects '10'" do
-    expect(div3).not_to accept("10")
-  end
-
-  it "accepts '11'" do
-    expect(div3).to accept("11")
-  end
-
-  it "rejects '100'" do
-    expect(div3).not_to accept("100")
+  it "correctly interprets a random string" do
+    prng = Random.new
+    rand0 = prng.rand(8)
+    rand1 = prng.rand(1000000)
+    input = '0'*rand0 + "%b" % rand1
+    if (rand1 % 3 == 0)
+      expect(div3).to accept(input)
+    else
+      expect(div3).to reject(input)
+    end
+    puts "\nrandom input:  #{input}"
+    puts "seed:  #{prng.seed}"
   end
 
 end
